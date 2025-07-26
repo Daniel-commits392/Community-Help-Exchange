03c1781c8022aee76810f44248109c1371a70148
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-07-26 18:29:59 +0300

    add navigation links in header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <header style={{ padding: "10px" }}>
             <h1>Community Help Exchange</h1>
-  
+            <nav>
+                <NavLink to ="/">All</NavLink>
+                <NavLink to="/requests">Requests</NavLink>
+                <NavLink to="/offers">Offers</NavLink>
+                <NavLink to="/newpost">New Post</NavLink>
+            </nav>
         </header>
     );
 }
