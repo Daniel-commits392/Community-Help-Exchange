7b45470c8f23f1e24ce0812e0406983e1e1561a1
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-07-26 18:22:17 +0300

    add functionality in postfoarm and rendered it in newpost

---
+++ src/Components/PostForm.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from "react";
+
+function PostForm() {
+   const [type, setType] = useState("offer");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [contact, setContact] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const newPost = {
+            type,
+            title,
+            description,
+            contact
+        };
+          fetch("http://localhost:4000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        alert("Post submitted!");
+        setTitle(""); 
+        setDescription("");
+        setType("offer");
+        setContact("");
+      })
+      .catch((err) => console.error("Submission error:", err));
+  }
+
+  return (
+         <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Type:
+          <select value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="offer">Offer</option>
+            <option value="request">Request</option>
+          </select>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Title:
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter post title"
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Description:
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Write your description..."
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Contact Info:
+          <input
+            value={contact}
+            onChange={(e) => setContact(e.target.value)}
+            placeholder="Email or phone"
+          />
+        </label>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default PostForm;
+    
--- src/Pages/NewPost.jsx
+++ src/Pages/NewPost.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import PostForm from "../Components/PostForm";
 
 function NewPost() {
     return (
         <div>
             <h2>Create a New Post</h2>
-       
+            <PostForm />
         </div>
     )
 }
