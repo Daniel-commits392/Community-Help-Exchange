aa36a4f3b6894f0b024842d7dbe54a947bf61ffc
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-07-26 19:37:55 +0300

    Requests component with fetch rendering

--- src/Pages/Requests.jsx
+++ src/Pages/Requests.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 
 function Requests () {
+    const [requests,setRequests]= useState([]);
+
+    useEffect (()=>{
+        fetch ("http://localhost:4000/posts")
+        .then(res=>res.json())
+        .then(data=>{
+            const filtered=data.filter(post=>post.type==="request")
+            setRequests(filtered)
+        })
+        .catch(err=>console.error("Error fetching requests",err));
+    },[]);
+    
     return (
         <div>
-            <h2>Requests</h2>
+            <h2>Requests</h2>         
+            {requests.length === 0 ? (
+                <p>No requests available at the moment.</p>
+                ):(
+                <ul>
+                    {requests.map((request)=>(
+                        <li key={request.id}>
+                        <h4>{request.title}</h4>
+                        <p>{request.description}</p>
+                        </li>
+                    ))}
+                        </ul>
+                        )}
             <p>Here you can find various requests from community members.</p>
         </div>
     );
