eb86aae9ffb800a1f2262e5262274f2dae3bcba3
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-07-26 18:33:55 +0300

    add code to home component as well as text

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to the Community Help Exchange</h2>
+            <p>Here you can find various requests and offers from community members.</p>
+            <p>Use the navigation links above to explore different sections.</p>
+        </div>
+    );
+}
+export default Home;
